44ad69cc1d9abe61028360ef946e232d12d27fd7
Veronica Sarahi Payan Estrada <18170418@itculiacan.edu.mx>
2022-11-13 15:11:08 -0600

    Agregando nodo de informacin individual

---
+++ laravel-vendetodo/resources/views/products/Individual.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('style')
+    <link rel="stylesheet" href="/css/productos-individual.css">
+@endsection
+
+@section('content')
+
+    <div class="">
+         @if($producto->getImagenUrl() == null)
+            <div class="card-image card-image-not-found"></div>
+        @else
+            <div class="">
+                <img src="{{ $producto->getImagenUrl() }}" alt="{{ $producto->getNombre() }}">
+            </div>
+        @endif
+        <article class="">
+                <h1 class="" title="{{ $producto->getNombre() }}">{{ $producto->getNombre() }}</h1>
+                @if($producto->getDescripcion() == null)
+                    <p class="">Sin descripción</p>
+                @else
+                    <p class="">{{ $producto->getDescripcion() }}</p>
+                @endif
+                <p>${{ number_format($producto->getPrecio(), 2) }}</p>
+                <label>Marca:</label>
+                <p>{{ $producto->getMarca()->getNombre() }}</p>
+                <label>Alto:</label>
+                <p>{{ $producto->getAlto() }}</p>
+                <label>Ancho:</label>
+                <p>{{ $producto->getAncho() }}</p>
+                <label>Largo:</label>
+                <p>{{ $producto->getLargo() }}</p>
+                <label>Proveedor:</label>
+                <select class="form-select">
+                <option value="" disabled selected>-- Sin seleccionar --</option>
+                @foreach ($marcas as $marca)
+                    <option value="{{$marca->getId()}}">{{$marca->getNombre()}}</option>
+                @endforeach
+                </select>
+                <button type="submit" class="btn btn-primary">Agregar al carrito</button>
+        </article>
+    </div>
+@endsection
