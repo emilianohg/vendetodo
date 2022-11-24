fe86fbb6c402f4429e7f9a02c013d5713efbef57
JuanPa505 <burguenojuanpa@gmail.com>
2022-11-23 21:49:01 -0700

    perfil de usuario bonito

--- laravel-vendetodo/resources/views/auth/perfil.blade.php
+++ laravel-vendetodo/resources/views/auth/perfil.blade.php
@@ -1,17 +1,34 @@
 @extends('layouts.app')
 
 @section('style')
-    <style>
-
-    </style>
+    <link rel="stylesheet" href="/css/perfil.css">
 @endsection
 
 @section('content')
 <div class="container">
-    Hola, {{ auth()->user()->nombre }}
-    <form action="{{ route('login.logout') }}" method="POST">
-        @csrf
-        <button type="submit">Cerrar sesión</button>
-    </form>
-</div>
+    <div class="profile-container">
+      <div class="img-user-container">
+        <img class="user-img" src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png" alt="">
+      </div>
+      <div class="info-user-container">
+        <div class="user-name">
+          <h2 class="tittle">Nombre de usuario:</h2>
+          <p>{{ auth()->user()->nombre }}</p>
+        </div>
+        <div class="user-email">
+          <h2 class="tittle">Correo:</h2>
+          <p>{{ auth()->user()->email }}</p>
+        </div>
+        <div class="user-role">
+          <h2 class="tittle">Rol:</h2>
+          <p>{{ auth()->user()->rol_id }}</p>
+        </div>
+        <div class="btn-container">
+              <form action="{{ route('login.logout') }}" method="POST">
+              @csrf
+                <button class="btn-user-logout" type="submit">Cerrar sesión</button>
+              </form>
+        </div>
+    </div>
+  </div>
 @endsection
